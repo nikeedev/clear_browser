80c830908a6a6626337a4467e5c351acf38ce8d3
nikeedev <nikita.gon.dev@gmail.com>
2023-12-05 08:55:10 +0100

    up

--- src/main.v
+++ src/main.v
@@ -4,13 +4,24 @@ import gg
 import gx
 import os
 import net.html
+import net.http
 import term
 
+
+struct Element {
+mut:
+	text string
+	size int // pixel font
+	width int
+	height int
+	element_type string
+}
 
 struct App {
 mut:
 	ctx    &gg.Context = unsafe { nil }
 	image []gg.Image
+	elements html.DocumentObjectModel
 }
 
 const (
@@ -27,31 +38,86 @@ fn main() {
 		println(term.cyan('\nUsage: clear_browser <HTML source code file>.html'))
 		exit(0)
 
-	} else {
-
-		file := os.read_lines(os.args[1]) !
-		mut html_file := ""
-
-		for line in file {
-			html_file += line.trim_space()
-			println(line)
-		}
-		println(html_file)
-
-		mut doc := html.parse(html_file)
-
-		println(doc)
 	}
-	// mut pos := playlib.Vec3{5, 5, 6}
-	// mut vecs := [Vec2{5, 5}, Vec2{6, 6}, Vec2{7, 7}]
-	// pos += playlib.Vec3{10, 10, 11}
 
-	// for vec in vecs {
-	// 	println(vec.str())
-	// }
+	if os.args[1].starts_with("https://") || os.args[1].starts_with("http://") {
+		println(term.green("HTTP requests to a website is not yet available"))
+		exit(0)
+	} else if !os.args[1].ends_with(".html") {
+		println(term.red("The file doesn't end with .html"))
+		exit(-1)
+	}
+
+	file := os.read_lines(os.args[1]) !
+	mut html_file := ""
+
+	for line in file {
+		html_file += line.trim_space()
+		println(line)
+	}
+	println(html_file)
+
+	mut doc := html.parse(html_file).get_tags()
+
+	mut elements := []Element{}
+	for i := 2; i < doc.len; i++ {
+		mut tag := doc[i]
+
+		mut element :=
+			match tag.name {
+				"h1" {
+					Element{
+						text: tag.text(),
+						size: 22
+						width: 6 * tag.text().len
+						height: 6
+						element_type: "h1"
+					}
+				}
+				"h2" {
+					Element{
+						text: tag.text(),
+						size: 18
+						width: 6 * tag.text().len
+						height: 6
+						element_type: "h1"
+					}
+				}
+				"h3" {
+					Element{
+						text: tag.text(),
+						size: 15
+						width: 6 * tag.text().len
+						height: 6
+						element_type: "h1"
+					}
+				}
+				"p" {
+					Element{
+						text: tag.text(),
+						size: 10
+						width: 6 * tag.text().len
+						height: 6
+						element_type: "h1"
+					}
+				}
+				else {
+					Element{
+						text: tag.text(),
+						size: 10
+						width: 6 * tag.text().len
+						height: 6
+						element_type: ""
+					}
+				}
+			}
+
+		elements.prepend(element)
+	}
 
 	mut app := &App{
-		ctx: 0
+		ctx: 0,
+		// elements: elements
 	}
 
 	app.ctx = gg.new_context(
@@ -62,10 +128,16 @@ fn main() {
 		window_title: win_title
 		frame_fn: frame
 		user_data: app
-		init_fn: init
+		init_fn: init,
+		resized_fn: resize_window
 	)
 
-	//app.ctx.run()
+	// app.ctx.run()
+}
+
+fn resize_window(e &gg.Event, mut app &App) {
+	app.ctx.width = e.window_width
+	app.ctx.height = e.window_height
 }
 
 fn init(mut app &App) {
@@ -73,18 +145,23 @@ fn init(mut app &App) {
 }
 
 // [live]
-fn (app &App) draw() {
-	mut rect := gg.Rect{x: 40, y: 40, width: 60, height: 40}
-
-	app.ctx.draw_rect_filled(rect.x, rect.y, rect.width, rect.height, gx.rgb(100, 149, 237))
-
-	app.ctx.draw_text_def(40+10, 40+12, 'game.')
+fn (app &App) draw(ctx &gg.Context) {
+	if ctx.pressed_keys[int(gg.KeyCode.escape)] {
+		exit(0)
+	}
 }
 
 
 fn frame(app &App) {
 	app.ctx.begin()
-	app.draw()
+
+	app.draw(app.ctx)
+
 	app.ctx.end()
+}
+
+
+fn render_html(doc html.DocumentObjectModel) {
+
 }
 
